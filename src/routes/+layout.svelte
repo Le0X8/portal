a024d83f6f7468888ca6fe838eac519e74c42440
Leonard Lesinski <leonard.lesinski@tu-dortmund.de>
2026-01-11 13:50:08 +0100

    some style changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,28 +9,30 @@
 
 <div class="min-h-screen bg-white text-black dark:bg-black dark:text-white">
 	<nav class="mx-auto flex w-full max-w-240 items-center p-4">
-		<div class="basis-1/6 text-center">
+		<div class="hidden basis-1/6 text-center lg:block">
 			<a href="/">Savegames</a>
 		</div>
-		<div class="basis-1/6 text-center">
-			<a href="https://store.steampowered.com/app/400/">Get Portal</a>
+		<div class="hidden basis-1/6 text-center lg:block">
+			<a href="https://n8s.uk/~st.400">Get Portal</a>
 		</div>
 
-		<div class="flex basis-2/6 items-center justify-center gap-2 text-2xl font-bold uppercase">
+		<div
+			class="flex w-full items-center justify-center gap-2 text-2xl font-bold uppercase lg:basis-2/6"
+		>
 			<img src="/aperture.svg" alt="" class="h-6" />
 			<a href="/">Aperture Science</a>
 		</div>
 
-		<div class="basis-1/6 text-center">
-			<a href="https://store.steampowered.com/app/620/">Get Portal 2</a>
+		<div class="hidden basis-1/6 text-center lg:block">
+			<a href="https://n8s.uk/~st.620">Get Portal 2</a>
 		</div>
-		<div class="basis-1/6 text-center">
-			<a href="https://github.com/Le0X8/portal">GitHub</a>
+		<div class="hidden basis-1/6 text-center lg:block">
+			<a href="https://n8s.uk/~gh.Le0X8.portal">GitHub</a>
 		</div>
 	</nav>
 
 	<main
-		class="mx-auto prose min-h-[calc(100vh-2rem)] max-w-240 p-4 prose-neutral dark:prose-invert"
+		class="mx-auto prose min-h-[calc(100vh-2rem)] max-w-240 p-4 prose-neutral has-[div]:max-w-none dark:prose-invert"
 	>
 		{@render children()}
 	</main>
@@ -41,6 +43,7 @@
 		<p class="mb-1 text-sm">
 			created by <a href="https://lesinski.cc/" class="underline">@Le0_X8</a>
 		</p>
+		<p class="text-xs">Image Copyright: Valve Corporation.</p>
 		<p class="text-xs">This is a fan-made project and not affiliated with Valve Corporation.</p>
 	</footer>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,20 @@
 	<title>Aperture Science</title>
 </svelte:head>
 
-<h1>Aperture Science</h1>
+<div class="w-full p-8">
+	<h1 class="mt-20 mb-30 text-6xl text-neutral-500">
+		&ldquo;We do what we must <br />
+		<span class="text-black dark:text-white">&nbsp;&nbsp;&nbsp;because we can.&rdquo;</span><br />
+		<span class="text-2xl"
+			>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&ndash; Cave Johnson</span
+		>
+	</h1>
 
-<p>
-	<span class="italic">&ldquo;We do what we must because we can.&rdquo;</span>
-	&ndash; Cave Johnson
-</p>
+	<p>On this site:</p>
 
-<ul>
-	<li>
-		<a href="/saves">Savegames</a>
-	</li>
-</ul>
+	<ul>
+		<li>
+			<a href="/saves">Savegames</a>
+		</li>
+	</ul>
+</div>
