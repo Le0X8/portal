730f2c22792bc44eb08df1b88e7b15f7244e0e4c
Leonard Lesinski <leonard.lesinski@tu-dortmund.de>
2026-01-11 13:29:17 +0100

    added first 5 chapters of Portal 2

--- src/routes/saves/+page.svelte
+++ src/routes/saves/+page.svelte
@@ -172,4 +172,126 @@
 
 <h2>Portal 2</h2>
 
-<p>Work in progess.</p>
+<ul>
+	<li>
+		<b>Chapter 1:</b>
+		The Courtesy Call
+		<ul>
+			<li>
+				Chamber 0
+				<i>(after Container Ride)</i>
+				<ul>
+					<li><a href={saveUrl('p2/c1-c0')}>Start</a></li>
+					<li><a href={saveUrl('p2/c1-c0-skip')}>Skip the dialogue</a></li>
+				</ul>
+			</li>
+			<li><a href={saveUrl('p2/c1-c1')}>Chamber 1</a> (Portal Carousel)</li>
+			<li><a href={saveUrl('p2/c1-c2')}>Chamber 2</a> (Portal Gun)</li>
+			<li><a href={saveUrl('p2/c1-c3')}>Chamber 3</a> (Smooth Jazz)</li>
+			<li><a href={saveUrl('p2/c1-c4')}>Chamber 4</a> (after Smooth Jazz)</li>
+			<li><a href={saveUrl('p2/c1-c5')}>Chamber 5</a> (Cube Momentum)</li>
+			<li>
+				Chamber 6
+				<i>(Future Starter)</i>
+				<ul>
+					<li><a href={saveUrl('p2/c1-c6')}>Room 1</a></li>
+					<li><a href={saveUrl('p2/c1-c6-room2')}>Room 2</a></li>
+				</ul>
+			</li>
+			<li>
+				Chamber 7
+				<i>(Secret Panel)</i>
+				<ul>
+					<li><a href={saveUrl('p2/c1-c7')}>Start</a></li>
+					<li><a href={saveUrl('p2/c1-c7-drop')}>Wheatley drops off the rail</a></li>
+					<li><a href={saveUrl('p2/c1-c7-open')}>Secret Panel opens</a></li>
+				</ul>
+			</li>
+			<li>
+				GLaDOS Chamber
+				<i>(Wakeup)</i>
+				<ul>
+					<li><a href={saveUrl('p2/c1-boss')}>Start</a></li>
+					<li><a href={saveUrl('p2/c1-boss-wakeup')}>GLaDOS wakes up</a></li>
+				</ul>
+			</li>
+			<li><a href={saveUrl('p2/c1-c19')}>Chamber 19</a> (Incinerator)</li>
+		</ul>
+	</li>
+	<li>
+		<b>Chapter 2:</b>
+		The Cold Boot
+		<ul>
+			<li><a href={saveUrl('p2/c2-c1')}>Chamber 1</a> (Laser Intro)</li>
+			<li><a href={saveUrl('p2/c2-c2')}>Chamber 2</a> (Laser Stairs)</li>
+			<li><a href={saveUrl('p2/c2-c3')}>Chamber 3</a> (Dual Lasers)</li>
+			<li><a href={saveUrl('p2/c2-c4')}>Chamber 4</a> (Laser Over Goo)</li>
+			<li><a href={saveUrl('p2/c2-c5')}>Chamber 5</a> (Catapult Intro)</li>
+			<li><a href={saveUrl('p2/c2-c6')}>Chamber 6</a> (Trust Fling)</li>
+			<li><a href={saveUrl('p2/c2-c7')}>Chamber 7</a> (Pit Flings)</li>
+			<li><a href={saveUrl('p2/c2-c8')}>Chamber 8</a> (Fizzler Intro)</li>
+		</ul>
+	</li>
+	<li>
+		<b>Chapter 3:</b>
+		The Return
+		<ul>
+			<li><a href={saveUrl('p2/c3-c9')}>Chamber 9</a> (Ceiling Catapult)</li>
+			<li><a href={saveUrl('p2/c3-c10')}>Chamber 10</a> (Ricochet)</li>
+			<li><a href={saveUrl('p2/c3-c11')}>Chamber 11</a> (Bridge Intro)</li>
+			<li><a href={saveUrl('p2/c3-c12')}>Chamber 12</a> (Bridge the Gap)</li>
+			<li><a href={saveUrl('p2/c3-c13')}>Chamber 13</a> (Turret Intro)</li>
+			<li><a href={saveUrl('p2/c3-c14')}>Chamber 14</a> (Laser Relays)</li>
+			<li><a href={saveUrl('p2/c3-c15')}>Chamber 15</a> (Turret Blocker)</li>
+			<li><a href={saveUrl('p2/c3-c16')}>Chamber 16</a> (Laser vs. Turret)</li>
+			<li><a href={saveUrl('p2/c3-c17')}>Chamber 17</a> (Pull the Rug)</li>
+		</ul>
+	</li>
+	<li>
+		<b>Chapter 4:</b>
+		The Surprise
+		<ul>
+			<li><a href={saveUrl('p2/c4-c18')}>Chamber 18</a> (Column Blocker)</li>
+			<li><a href={saveUrl('p2/c4-c19')}>Chamber 19</a> (Laser Chaining)</li>
+			<li><a href={saveUrl('p2/c4-c20')}>Chamber 20</a> (Triple Laser)</li>
+			<li><a href={saveUrl('p2/c4-c21')}>Chamber 21</a> (Jailbreak)</li>
+			<li><a href={saveUrl('p2/c4-e1')}>Escape Part 1</a></li>
+			<li><a href={saveUrl('p2/c4-e2')}>Escape Part 2</a></li>
+		</ul>
+	</li>
+	<li>
+		<b>Chapter 5:</b>
+		The Escape
+		<ul>
+			<li>
+				Turret Manufacturing
+				<i>(Turret Factory)</i>
+				<ul>
+					<li><a href={saveUrl('p2/c5-e1')}>Obstacle 1</a></li>
+					<li><a href={saveUrl('p2/c5-e1-obst2')}>Obstacle 2</a></li>
+				</ul>
+			</li>
+			<li>
+				Turret Control Center
+				<i>(Turret Sabotage)</i>
+				<ul>
+					<li><a href={saveUrl('p2/c5-e2')}>Obstacle 1</a></li>
+					<li><a href={saveUrl('p2/c5-e2-obst2')}>Obstacle 2</a></li>
+				</ul>
+			</li>
+			<li><a href={saveUrl('p2/c5-e3')}>Neurotoxin Generator</a> (Neurotoxin Sabotage)</li>
+			<li><a href={saveUrl('p2/c5-pipe')}>Tube Ride</a></li>
+			<li>
+				GLaDOS' Lair
+				<i>(Core)</i>
+				<ul>
+					<li><a href={saveUrl('p2/c5-cat')}>Tube Exit</a></li>
+					<li><a href={saveUrl('p2/c5-boss')}>GLaDOS' Lair</a></li>
+					<li><a href={saveUrl('p2/c5-stalemate')}>Stalemate</a></li>
+				</ul>
+			</li>
+		</ul>
+	</li>
+</ul>
+
+To be continued... (I still have 62 savegames ready to put them here)
